c359aefb3086f8c03373bf5f0e727e0726082352
Conor Toomb <conor.toomb114@gmail.com>
2020-04-03 23:41:57 -0400

    workouts and routes

---
+++ seeders/seed.js
@@ -0,0 +1,147 @@
+let mongoose = require("mongoose");
+let db = require("../models");
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+let workoutSeed = [
+  {
+    day: new Date().setDate(new Date().getDate()-10),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bicep Curl",
+        duration: 20,
+        weight: 75,
+        reps: 5,
+        sets: 3
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-9),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Leg Press",
+        duration: 20,
+        weight: 400,
+        reps: 8,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-8),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Push Press",
+        duration: 25,
+        weight: 105,
+        reps: 10,
+        sets: 5
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-7),
+    exercises: [
+      {
+        type: "cardio",
+        name: "Running",
+        duration: 30,
+        distance: 3
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-6),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench Press",
+        duration: 35,
+        weight: 195,
+        reps: 12,
+        sets: 3
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-5),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Incline Press",
+        duration: 35,
+        weight: 175,
+        reps: 12,
+        sets: 3
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-4),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Power Cleans",
+        duration: 25,
+        weight: 240,
+        reps: 12,
+        sets: 3
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-3),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Back Squat",
+        duration: 20,
+        weight: 350,
+        reps: 12,
+        sets: 3
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-2),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Lunges",
+        duration: 30,
+        weight: 200,
+        reps: 12,
+        sets: 3
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-1),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Plank",
+        duration: 40,
+        distance: 3
+      }
+    ]
+  }
+];
+
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
